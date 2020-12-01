3706739d4bdc170b3f82a6e082b8df74cbbacfa6
Arpan Neupane <arpanneupane19@gmail.com>
2020-11-30 23:08:12 -0600

    added mailto link (working)

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Email.css';
 
 
 function Email() {
+
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [emailSubject, setEmailSubject] = useState('');
+	const [emailBody, setEmailBody] = useState('');
+
+	const mailLink = `mailto:arpanneupane19@gmail.com?subject=${emailSubject}&body=${emailBody}`;
 	return (
+
 	<div className='app'>
 		<div className='header'>
 			<h2>Email me!</h2>
 		</div>
 		<div className='form'>
-			<form method='POST'>
-				<input type='text' placeholder='First Name'></input>
-				<input type='text' placeholder='Last Name'></input>
+			<form>
+
+				
+					<input type='text' value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder='First Name'></input>
+					<input type='text' value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder='Last Name'></input>
+
+				
 				<div className='email'>
-					<input type='text' placeholder='Your Email Address'></input>
-					<textarea placeholder='Email Body'></textarea>
+					<input value={emailSubject} onChange={(e) => setEmailSubject(e.target.value)} placeholder='Email Subject'></input>
+					<textarea value={emailBody} onChange={(e) => setEmailBody(e.target.value)} placeholder='Email Body'></textarea>
 				</div>
 				<div className='submit'>
-					<input type='submit' value='Send Email'></input>
+					<a href={mailLink} target='__blank'>Send Email</a>
 				</div>
+				
 			</form>
+				
 
 		</div>
 	</div>
